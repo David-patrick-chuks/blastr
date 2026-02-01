c51684abc7a220cbb92025f29d8263c8914cf5ea
David-patrick-chuks <pd3072894@gmail.com>
2026-02-01 11:55:32 +0100

    Fix: Correct service exports in index.ts

--- web/src/services/index.ts
+++ web/src/services/index.ts
@@ -1,5 +1,5 @@
-export * from './agentService.js';
+export * from './authService.js';
+export * from './campaignService.js';
 export * from './knowledgeService.js';
 export * from './chatService.js';
 export * from './analyticsService.js';
-export * from './authService.js';
